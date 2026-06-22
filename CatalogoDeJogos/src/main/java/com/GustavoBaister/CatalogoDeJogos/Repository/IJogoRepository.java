d1821356882c7ff6058ef3cbe141b7db8789bc0a
Baister <gustavobaister@gmail.com>
2026-06-22 20:40:12 -0300

    Refactor: ajustando a notation

--- CatalogoDeJogos/src/main/java/com/GustavoBaister/CatalogoDeJogos/Repository/IJogoRepository.java
+++ CatalogoDeJogos/src/main/java/com/GustavoBaister/CatalogoDeJogos/Repository/IJogoRepository.java
@@ -2,6 +2,8 @@ package com.GustavoBaister.CatalogoDeJogos.Repository;
 
 import com.GustavoBaister.CatalogoDeJogos.Model.Jogo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface IJogoRepository extends JpaRepository<Jogo, Long> {
 }
